39f6653a2618c71cc75c55698340ace5ca8ecb13
sergey30 <sergey30@users.noreply.github.com>
2018-04-19 00:45:32 +0700

    видео воспроизводится с определенной секунды
    минута местного времени == секунда видео

---
+++ templates/webinar.php
@@ -0,0 +1,47 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="assets/css/bootstrap-reboot.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <title>Webinar</title>
+  </head>
+  <body>
+    <div class="webinar-container">
+        <div class="header">
+            <div class="author">
+                <span class="faded-letters">Ведущие вебинара:</span>
+                <span class="author-name">Варвара Еремина. Эксперт в области таргетированной рекламы в соц. сетях. С 2015 года.</span>
+            </div>
+            <div class="participant">
+                <span class="faded-letters">Участник:</span>
+                <span class="participant-name">xcvc</span>
+                <button type="button" name="button">Выйти</button>
+            </div>
+        </div>
+        <div class="title">
+            "Турбо Инстаграм. Трансляция мастер-класса. Начинаем сегодня в 19:00 мск."
+        </div>
+        <div class="content">
+            <div class="img visible">
+                <img class="visible" src="assets/images/slide1.jpg" alt="">
+                <img class="" src="assets/images/slide2.jpg" alt="">
+                <img class="" src="assets/images/slide3.jpg" alt="">
+                <img class="" src="assets/images/slide4.jpg" alt="">
+                <div class="caption">Вебинар стартует в 19:00 мск Поддержка start@makers.bz</div>
+            </div>
+            <div class="video ">
+                <video controls></video>
+                <div class="caption">Если трансляция не идет, измените настройки браузера разрешив автозапуск контента</div>
+            </div>
+            <div class="chat">
+
+            </div>
+        </div>
+    </div>
+
+    <script src="assets/lib/jquery-3.3.1.js"></script>
+    <script src="assets/js/script.js"></script>
+  </body>
+</html>
